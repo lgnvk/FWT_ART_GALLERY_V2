96f77f66fed8442426bf90727c126e3459401e26
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-08-17 17:30:53 +0400

    reposition App Component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Card from '../Card';
+
+const App = () => {
+  return (
+    <div className="App">
+      Smth
+      <Card />
+    </div>
+  );
+};
+
+export default App;
