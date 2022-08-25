514541ac805fb154c85ee27d4fad6e17599cfe72
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-08-25 17:06:55 +0400

    props fixed

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,18 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import './Link.scss';
 import cn from 'classnames';
 
 interface LinkProps {
   theme: 'dark' | 'light';
   link: string;
-  value: string;
+  children: ReactNode;
 }
-const Link: FC<LinkProps & HTMLAnchorElement> = ({ theme, link, value }) => {
+const Link: FC<LinkProps & HTMLAnchorElement> = ({ theme, link, children }) => {
   const styles = cn('wrapper', theme);
   return (
     <div className={styles}>
       <a href={link} className="link">
-        {value}
+        {children}
       </a>
       <div className="underline" />
     </div>
