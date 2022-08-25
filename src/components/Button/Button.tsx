c743a01329b1af2713ea6b0cf12d01ef9341f0e8
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-08-25 12:27:55 +0400

    Button conversation fixex

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,21 +2,23 @@ import React, { FC } from 'react';
 import cn from 'classnames';
 import './Button.scss';
 
-interface ButtonProps {
+type ButtonProps = {
   handleClick: () => void;
-  theme: string;
-  buttonType: string;
-}
+  theme: 'dark' | 'light';
+  buttonType: 'outlined' | 'filled';
+  value: string;
+};
 
 const Button: FC<ButtonProps & HTMLButtonElement> = ({
   handleClick,
   theme,
   buttonType,
+  value,
 }) => {
   const styles = cn('button', theme, buttonType);
   return (
     <button type="button" onClick={() => handleClick} className={styles}>
-      Button
+      {value}
     </button>
   );
 };
