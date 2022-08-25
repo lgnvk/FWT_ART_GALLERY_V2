29f2d71849048d6f10863573123f227f14784c0c
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-08-25 11:06:09 +0400

    Card pull fixex

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from 'react';
-
+import cn from 'classnames';
 import './Card.scss';
 
-interface CardProps {
+type CardProps = {
   title: string;
   name: string;
   imgUrl: string;
   year: string;
-}
+  classnames: string;
+};
 
-const Card: FC<CardProps> = ({ title, imgUrl, name, year }) => {
+const Card: FC<CardProps> = ({ title, imgUrl, name, year, classnames }) => {
+  const styles = cn('card', classnames);
   return (
-    <div className="card">
+    <div className={styles}>
       <img src={imgUrl} alt="smth" className="card__img" />
       <div className="card__info">
         <div className="card__title">{title}</div>
