383c512f6f1bd04d2722ec99a9c0f1d8af901e51
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-09-21 11:22:42 +0400

    Порядок импортов

--- src/components/Modals/Menu/Menu.tsx
+++ src/components/Modals/Menu/Menu.tsx
@@ -1,11 +1,11 @@
 import React, { FC, useContext } from 'react';
 import cn from 'classnames/bind';
 import { ThemeContext } from '../../../context';
+import Button from '../../UI/Button';
 import { ThemeType } from '../../../types/types';
 import { ReactComponent as Close } from '../../../assets/img/svg/CloseIcon.svg';
 import { ReactComponent as ThemeDark } from '../../../assets/img/svg/ThemeSun.svg';
 import { ReactComponent as ThemeLight } from '../../../assets/img/svg/ThemeMoon.svg';
-import Button from '../../UI/Button';
 import styles from './Menu.scss';
 
 type MenuType = {
